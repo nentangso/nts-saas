b35ba56513f6453ac932e16eed2f69560dc6ea19
tientq <tientq@outlook.com>
2022-12-01 10:03:26 +0700

    Make zalando optional

--- nts-saas-framework-parent/nts-saas-core/src/main/java/org/nentangso/core/web/rest/errors/InvalidPasswordException.java
+++ nts-saas-framework-parent/nts-saas-core/src/main/java/org/nentangso/core/web/rest/errors/InvalidPasswordException.java
@@ -1,14 +1,11 @@
 package org.nentangso.core.web.rest.errors;
 
-import org.zalando.problem.AbstractThrowableProblem;
-import org.zalando.problem.Status;
-
 @SuppressWarnings("java:S110") // Inheritance tree of classes should not be too deep
-public class InvalidPasswordException extends AbstractThrowableProblem {
+public class InvalidPasswordException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
     public InvalidPasswordException() {
-        super(NtsErrorConstants.INVALID_PASSWORD_TYPE, "Incorrect password", Status.BAD_REQUEST);
+        super("Incorrect password");
     }
 }
