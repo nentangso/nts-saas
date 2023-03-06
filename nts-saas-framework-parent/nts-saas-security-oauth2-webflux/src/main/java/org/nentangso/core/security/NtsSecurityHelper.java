fda141dd0cf6cef67e937db77e5bc73176a78f1d
tientq <tientq@outlook.com>
2023-03-06 17:11:00 +0700

    Add security helper

---
+++ nts-saas-framework-parent/nts-saas-security-oauth2-webflux/src/main/java/org/nentangso/core/security/NtsSecurityHelper.java
@@ -0,0 +1,142 @@
+package org.nentangso.core.security;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Helper class for Spring Security.
+ */
+@Component
+public class NtsSecurityHelper implements InitializingBean {
+
+    private final String rolesClaim;
+    private final String rolePrefix;
+
+    private NtsSecurityHelper(
+        @Value("${nts.security.oauth2.client.configuration.roles-claim:roles}") String rolesClaim,
+        @Value("${nts.security.oauth2.client.configuration.role-prefix:ROLE_}") String rolePrefix
+    ) {
+        this.rolesClaim = rolesClaim;
+        this.rolePrefix = rolePrefix;
+    }
+
+    /**
+     * Get the login of the current user.
+     *
+     * @return the login of the current user.
+     */
+    public Mono<String> getCurrentUserLogin() {
+        return ReactiveSecurityContextHolder
+            .getContext()
+            .map(SecurityContext::getAuthentication)
+            .flatMap(authentication -> Mono.justOrEmpty(extractPrincipal(authentication)));
+    }
+
+    private String extractPrincipal(Authentication authentication) {
+        if (authentication == null) {
+            return null;
+        } else if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            return springSecurityUser.getUsername();
+        } else if (authentication instanceof JwtAuthenticationToken) {
+            return (String) ((JwtAuthenticationToken) authentication).getToken().getClaims().get("preferred_username");
+        } else if (authentication.getPrincipal() instanceof DefaultOidcUser) {
+            Map<String, Object> attributes = ((DefaultOidcUser) authentication.getPrincipal()).getAttributes();
+            if (attributes.containsKey("preferred_username")) {
+                return (String) attributes.get("preferred_username");
+            }
+        } else if (authentication.getPrincipal() instanceof String) {
+            return (String) authentication.getPrincipal();
+        }
+        return null;
+    }
+
+    /**
+     * Check if a user is authenticated.
+     *
+     * @return true if the user is authenticated, false otherwise.
+     */
+    public Mono<Boolean> isAuthenticated() {
+        return ReactiveSecurityContextHolder
+            .getContext()
+            .map(SecurityContext::getAuthentication)
+            .map(Authentication::getAuthorities)
+            .map(authorities -> authorities.stream().map(GrantedAuthority::getAuthority).noneMatch(NtsAuthoritiesConstants.ANONYMOUS::equals));
+    }
+
+    /**
+     * Checks if the current user has any of the authorities.
+     *
+     * @param authorities the authorities to check.
+     * @return true if the current user has any of the authorities, false otherwise.
+     */
+    public Mono<Boolean> hasCurrentUserAnyOfAuthorities(String... authorities) {
+        return ReactiveSecurityContextHolder
+            .getContext()
+            .map(SecurityContext::getAuthentication)
+            .map(Authentication::getAuthorities)
+            .map(authorityList ->
+                authorityList
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .anyMatch(authority -> Arrays.asList(authorities).contains(authority))
+            );
+    }
+
+    /**
+     * Checks if the current user has none of the authorities.
+     *
+     * @param authorities the authorities to check.
+     * @return true if the current user has none of the authorities, false otherwise.
+     */
+    public Mono<Boolean> hasCurrentUserNoneOfAuthorities(String... authorities) {
+        return hasCurrentUserAnyOfAuthorities(authorities).map(result -> !result);
+    }
+
+    /**
+     * Checks if the current user has a specific authority.
+     *
+     * @param authority the authority to check.
+     * @return true if the current user has the authority, false otherwise.
+     */
+    public Mono<Boolean> hasCurrentUserThisAuthority(String authority) {
+        return hasCurrentUserAnyOfAuthorities(authority);
+    }
+
+    public List<GrantedAuthority> extractAuthorityFromClaims(Map<String, Object> claims) {
+        return mapRolesToGrantedAuthorities(getRolesFromClaims(claims));
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<String> getRolesFromClaims(Map<String, Object> claims) {
+        return (Collection<String>) claims.getOrDefault(rolesClaim, new ArrayList<>());
+    }
+
+    private List<GrantedAuthority> mapRolesToGrantedAuthorities(Collection<String> roles) {
+        return roles.stream().filter(role -> role.startsWith(rolePrefix)).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private static NtsSecurityHelper instance;
+
+    @Override
+    public void afterPropertiesSet() {
+        instance = this;
+    }
+
+    public static NtsSecurityHelper getInstance() {
+        return instance;
+    }
+}
