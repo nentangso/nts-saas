888422a2cd82e7a9adf2301c681ce8f46e1f5767
tientq <tientq@outlook.com>
2023-03-10 16:19:17 +0700

    Add @AuthorizedFeignClient

---
+++ nts-saas-framework-parent/nts-saas-security-oauth2/src/main/java/org/nentangso/core/client/OAuth2InterceptedFeignConfiguration.java
@@ -0,0 +1,13 @@
+package org.nentangso.core.client;
+
+import feign.RequestInterceptor;
+import org.nentangso.core.security.oauth2.AuthorizationHeaderUtil;
+import org.springframework.context.annotation.Bean;
+
+public class OAuth2InterceptedFeignConfiguration {
+
+    @Bean(name = "oauth2RequestInterceptor")
+    public RequestInterceptor getOAuth2RequestInterceptor(AuthorizationHeaderUtil authorizationHeaderUtil) {
+        return new TokenRelayRequestInterceptor(authorizationHeaderUtil);
+    }
+}
