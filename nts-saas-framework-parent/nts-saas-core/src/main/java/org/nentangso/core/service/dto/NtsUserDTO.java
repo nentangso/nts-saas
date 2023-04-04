bc728bdcf0c45a712b28576604cef479632f4856
tientq <tientq@outlook.com>
2023-04-04 10:16:02 +0700

    Add more fields to public user api

--- nts-saas-framework-parent/nts-saas-core/src/main/java/org/nentangso/core/service/dto/NtsUserDTO.java
+++ nts-saas-framework-parent/nts-saas-core/src/main/java/org/nentangso/core/service/dto/NtsUserDTO.java
@@ -1,6 +1,12 @@
 package org.nentangso.core.service.dto;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A DTO representing a user, with only the public attributes.
@@ -12,6 +18,13 @@ public class NtsUserDTO implements Serializable {
     private String id;
 
     private String login;
+
+    private String firstName;
+
+    private String lastName;
+
+    private boolean activated = false;
+
 
     public NtsUserDTO() {
         // Empty constructor needed for Jackson.
@@ -33,12 +46,47 @@ public class NtsUserDTO implements Serializable {
         this.login = login;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDisplayName() {
+        return Stream.of(firstName, lastName)
+            .map(StringUtils::trimToNull)
+            .filter(Objects::nonNull)
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.joining(" "));
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
     // prettier-ignore
     @Override
     public String toString() {
-        return "UserDTO{" +
+        return "NtsUserDTO{" +
             "id='" + id + '\'' +
             ", login='" + login + '\'' +
-            "}";
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", activated=" + activated +
+            '}';
     }
 }
