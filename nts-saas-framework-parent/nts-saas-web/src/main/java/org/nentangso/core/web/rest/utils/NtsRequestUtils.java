004a69c5658bbf46c7d568574b72a51889edcfb3
tientq <tientq@outlook.com>
2023-04-28 15:49:48 +0700

    Fix NtsRequestUtils.getBody

--- nts-saas-framework-parent/nts-saas-web/src/main/java/org/nentangso/core/web/rest/utils/NtsRequestUtils.java
+++ nts-saas-framework-parent/nts-saas-web/src/main/java/org/nentangso/core/web/rest/utils/NtsRequestUtils.java
@@ -2,17 +2,30 @@ package org.nentangso.core.web.rest.utils;
 
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
 import java.io.IOException;
+import java.util.Optional;
 
 @SuppressWarnings("unused")
 public class NtsRequestUtils {
     private NtsRequestUtils() {
     }
 
-    public static String getBody(HttpServletRequest request) throws IOException {
-        ContentCachingRequestWrapper wrappedRequest = (ContentCachingRequestWrapper) request;
+    public static String getBody(ServletRequest request) throws IOException {
+        ContentCachingRequestWrapper wrappedRequest = getWrappedRequest(request)
+            .orElseThrow(() -> new UnsupportedOperationException("Application filters is not configure correctly."));
         byte[] bytes = wrappedRequest.getContentAsByteArray();
         return new String(bytes, wrappedRequest.getCharacterEncoding());
+    }
+
+    private static Optional<ContentCachingRequestWrapper> getWrappedRequest(ServletRequest request) {
+        if (request instanceof ContentCachingRequestWrapper) {
+            return Optional.of((ContentCachingRequestWrapper) request);
+        }
+        if (request instanceof ServletRequestWrapper) {
+            return getWrappedRequest(((ServletRequestWrapper) request).getRequest());
+        }
+        return Optional.empty();
     }
 }
