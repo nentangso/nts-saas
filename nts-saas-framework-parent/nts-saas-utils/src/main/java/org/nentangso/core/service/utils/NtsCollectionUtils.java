2a4d7a776de4fce77e3d9442f1fbc2bb575ab53d
tientq <tientq@outlook.com>
2023-05-05 15:56:19 +0700

    Add NtsCollectionUtils.chunks

--- nts-saas-framework-parent/nts-saas-utils/src/main/java/org/nentangso/core/service/utils/NtsCollectionUtils.java
+++ nts-saas-framework-parent/nts-saas-utils/src/main/java/org/nentangso/core/service/utils/NtsCollectionUtils.java
@@ -2,10 +2,23 @@ package org.nentangso.core.service.utils;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public class NtsCollectionUtils {
     private NtsCollectionUtils() {
+    }
+
+    public static <T> Stream<List<T>> chunks(List<T> source, int length) {
+        if (length <= 0)
+            throw new IllegalArgumentException("length = " + length);
+        int size = source.size();
+        if (size <= 0)
+            return Stream.empty();
+        int fullChunks = (size - 1) / length;
+        return IntStream.range(0, fullChunks + 1).mapToObj(
+            n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
     }
 
     public static List<String> normalize(Collection<?> collection) {
